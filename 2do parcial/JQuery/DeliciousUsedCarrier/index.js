c96dc8a759a3da3648f2c6f69ff0a82e88363483
Adrian Garcia <adriangarlop@hotmail.com>
2019-09-27 07:43:07 -0500

    finished DeliciousUsedCarrier practice

--- 2do parcial/JQuery/DeliciousUsedCarrier/index.js
+++ 2do parcial/JQuery/DeliciousUsedCarrier/index.js
@@ -6,23 +6,19 @@ function main() {
   for (let i=0; i<cats.length; i++) {
 
     cats[i].addEventListener("click", (event) => {
-
       event.preventDefault();
 
-      if (i == 0) {
-        $(".hero")
-      }
+      if (i == 0) 
+        $(".hero").children().attr('src', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-65/cat1.jpg');
 
       if (i == 1)
-        console.log(i);
+        $(".hero").children().attr('src', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-65/cat2.jpg');
 
       if (i == 2)
-        console.log(i);
+        $(".hero").children().attr('src', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-65/cat3.jpg');
 
-      if (i == 3)
-        console.log(i);
-
-
+      if (i == 3) 
+        $(".hero").children().attr('src', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-65/cat4.jpg');
     }); 
   }
 
